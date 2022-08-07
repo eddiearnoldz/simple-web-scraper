c76b52ec41fb45e8030beecadd035e557305aaaf
eddiearnoldz <eddiearnoldz@me.com>
2022-08-07 20:03:12 +0100

    changed the first api to scrape bbc football for headlines

--- scraperapi.js
+++ scraperapi.js
@@ -1,11 +1,18 @@
 const axios = require ('axios')
 const cheerio = require('cheerio')
-const url = 'https://www.turmerry.com/collections/organic-cotton-sheet-sets/products/percale-natural-color-organic-sheet-sets';
+const url = 'https://www.bbc.co.uk/sport/football';
+const headlines = []
 axios(url)
 .then(response => {
   const html = response.data;
   const $ = cheerio.load(html)
-  const newPrice = $('.new-price').text()
-  console.log(newPrice)
+  const newPrice = $('.gs-c-promo-heading').text()
+  const seperatedTitles = newPrice.split(/(?<!\s)(?=[A-Z])/)
+  seperatedTitles.forEach(title => {
+    headlines.push({
+      'title': title
+    })
+  })
+  console.log(headlines)
 })
 .catch(console.error)
